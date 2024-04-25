8ad41325ad732a10e7ef795ee112bbcb6fbe464f
phukaoz <tnpkofficial@gmail.com>
2024-04-26 00:22:50 +0700

    fix: change macro

--- include/pair.h
+++ include/pair.h
@@ -1,15 +1,19 @@
 #ifndef _PAIR_H_
 #define _PAIR_H_
 
-#ifndef pair_t
-#define pair_t int
+#ifndef pair_t1
+#define pair_t1 int
+#endif // pair_t
+
+#ifndef pair_t2
+#define pair_t2 int
 #endif // pair_t
 
 typedef struct{
-    pair_t first;
-    pair_t second;
+    pair_t1 first;
+    pair_t2 second;
 }pair;
 
-pair pair_make(pair_t first, pair_t second);
+pair pair_make(pair_t1 first, pair_t2 second);
 
 #endif // pair.h
--- src/pair.c
+++ src/pair.c
@@ -1,6 +1,6 @@
 #include "pair.h"
 
-pair pair_make(pair_t _first, pair_t _second){
+pair pair_make(pair_t1 _first, pair_t2 _second){
     pair new_pair;
     new_pair.first = _first;
     new_pair.second = _second;
