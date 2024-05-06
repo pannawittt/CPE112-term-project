a9f936a2ab9c70b027dfb48af3f796d3831b1e1e
Pannawit Wantae <pnwwantae60@gmail.com>
2024-05-06 11:57:09 +0700

    add recent busstop fn

--- ui/mainUI.c
+++ ui/mainUI.c
@@ -19,9 +19,9 @@ void printUnderBorder() {
     printf("\n");
 }
 
-int RemainingSpace(char *str) {
+int RemainingSpace(char *str, int space) {
     int len = strlen(str);
-    int remainingSpace = 27 - len;
+    int remainingSpace = space - len;
 
     return remainingSpace;
 }
@@ -30,7 +30,7 @@ void inputCHAR(char *CHAR) {
     printf("%c\t", 179);
     fflush(stdout);
     scanf("%s", CHAR);
-    printf("\033[A\033[K%c\t %s%*s%c\n", 179, CHAR, RemainingSpace(CHAR), "", 179);
+    printf("\033[A\033[K%c\t %s%*s%c\n", 179, CHAR, RemainingSpace(CHAR, 27), "", 179);
 }
 
 void inputINT(int *INT) {
@@ -40,10 +40,28 @@ void inputINT(int *INT) {
     // Convert integer to character
     char charValue[1000];
     sprintf(charValue, "%d", *INT);
-    printf("\033[A\033[K%c\t %s%*s%c\n", 179, charValue, RemainingSpace(charValue), "", 179);
+    printf("\033[A\033[K%c\t %s%*s%c\n", 179, charValue, RemainingSpace(charValue, 27), "", 179);
 }
 
-char src[1000], dest[1000];
+void adaptiveWindowsTree(char *CHAR) {
+    printf("%c   %c", 179, 195);
+    fflush(stdout);
+    scanf("%s", CHAR);
+    printf("\033[A\033[K%c   %c%s%*s%c\n", 179, 195,  CHAR, RemainingSpace(CHAR, 31), "", 179);
+}
+
+
+void adaptiveWindowsLastChild(char *CHAR) {
+    printf("%c   %c", 179, 192);
+    fflush(stdout);
+    scanf("%s", CHAR);
+    printf("\033[A\033[K%c   %c%s%*s%c\n", 179, 192,  CHAR, RemainingSpace(CHAR, 31), "", 179);
+
+}
+
+
+
+char src[1000], dest[1000], recentBusStop[1000][1000];
 int time, range;
 
 void printBody() {
@@ -58,18 +76,18 @@ void printBody() {
     inputCHAR(src);
     printf("%c  %c  Where're u going?             %c\n", 179, 175,179);
     inputCHAR(dest);
-    printf("%c  %c  What time is it?              %c\n", 179, 232, 179);
+    printf("%c  %c  What time is it? (hhmm)       %c\n", 179, 232, 179);
     inputINT(&time);
-    printf("%c  %c%c How long ur walkable range?   %c\n", 179, 168, 63, 179);
+    printf("%c  %c%c range ur walkable? (m)        %c\n", 179, 168, 63, 179);
     inputINT(&range);
     printf("%c                                   %c\n", 179, 179);
     printf("%c                                   %c\n", 179, 179);
     printf("%c > Recent bus stop you used        %c\n", 179, 179);
-    printf("%c                                   %c\n", 179, 179);
-    printf("%c                                   %c\n", 179, 179);
-    printf("%c                                   %c\n", 179, 179);
-    printf("%c                                   %c\n", 179, 179);
-    printf("%c                                   %c\n", 179, 179);
+    adaptiveWindowsTree(recentBusStop[0]);
+    adaptiveWindowsTree(recentBusStop[1]);
+    adaptiveWindowsTree(recentBusStop[2]);
+    adaptiveWindowsTree(recentBusStop[3]);
+    adaptiveWindowsLastChild(recentBusStop[4]);
     printf("%c                                   %c\n", 179, 179);
     printf("%c                                   %c\n", 179, 179);
     printf("%c                                   %c\n", 179, 179);
