2c2f10c0f6c23dad75f41b4aaa16cd807391ce2b
phukaoz <tnpkofficial@gmail.com>
2024-05-08 21:12:57 +0700

    refactor(init): use memcpy()

--- include/graph.h
+++ include/graph.h
@@ -1,19 +1,9 @@
 #ifndef _GRAPH_H_
 #define _GRAPH_H_
 
-#include "util.h"
+typedef struct graph *graph;
 
-typedef struct{
-   int weight;
-   int bus;
-}path;
-
-typedef struct{
-    int nodeSize;
-    path** adjMatrix;
-}graph;
-
-graph* graph_create(const int _nodeSize);
-void graph_addedge(graph* _graph, const int _sourceVertex, const int _destinaionVertex, const int _weight, const int _busNo);
+graph graph_create(const int _nodeSize);
+void graph_addedge(graph _graph, const int _sourceVertex, const int _destinaionVertex, int _weight, char *_busNo);
 
 #endif //graph.h
