8e1a012baf2aa420e2a8c6fc4f1a829d20fe60b3
phukaoz <tnpkofficial@gmail.com>
2024-05-11 14:13:28 +0700

    update: finishing

--- src/main.c
+++ src/main.c
@@ -1,8 +1,9 @@
-// define & macro
+/* define & typedef */
 #define DEBUG
-#define MAXCHAR 250
+#define MAXCHAR (sizeof(char)*250)
+typedef struct edge *edge;
 
-// นำเข้า library
+/* include library */
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -12,48 +13,55 @@
 #include "vector.h"
 #include "heap.h"
 #include "map.h"
-#include "graph.h"
 
-/* global variable */
+/* global variable & struct */
 char userTime[10] = "0550";
+char userSrc[MAXCHAR] = "วัดสุทธิ";
+char userDest[MAXCHAR] = "วัดยานนาวา";
+int walkLength = 0;
+struct edge{
+	int weight;
+	size_t vertex;
+	char* busNo;
+};
 
-/* declared function */
+/* function */
 void runMain();
 void runTest();
 int keyCmpr(const void* a, const void* b); 		// ฟังก์ชั่นเปรียบเทียบ key ใน map
-int timeSift(char **a); 							  // กรอง row ใน timetable
-
-void bi(void *c, size_t soe){
-	unsigned char *p = c;
-	while(soe--){
-		for( int i = 7; i >= 0; i-- ) {
-    		printf( "%d", ( *p++ >> i ) & 1 ? 1 : 0 );
-		}
-		printf("\n");
-	}
-}
+int timeSift(char **a); 						// กรอง row ใน timetable
+int itemCmpr(const void* a, const void* b);
+edge edge_create(int weight, size_t vertex, char* busNo);
 
 /* main function */
-int main(){
-	runMain();
-	//runTest();
-	printf("\n\n ---- FINISH ---- \n\n");
+int main(int argc, char **argv){	
+	if(argc == 1){
+		runMain();
+	}else if(strcmp(argv[0], "test")){
+		printf("------- TEST MODE ------- \n\n");
+		runTest();
+	}
+	printf("\n\n ------- FINISH ------- \n\n");
 }
 
 /* defined function */
 void runMain(void){
 	vector signMap;
 	mat timetable, ntimetable;
+	size_t nodeNum;
 
 	/* input จากผู้ใช้งาน */
 	printf("ตอนนี้กี่โมง? : ");scanf("%4s",userTime);
+	//printf("ตอนนี้อยู่ไหน? : ");scanf("%s",userSrc);
+	//printf("จะไปไหน? : ");scanf("%s",userDest);
+	printf("เดินได้เยอะแค่ไหน? : ");scanf("%d",&walkLength);
 
-	timetable = mat_readcsv("data/bus_timetable.csv");                      // อ่านไฟล์ timetable.csv
+	timetable = mat_readcsv("data/bus_timetable.csv");                   // อ่านไฟล์ timetable.csv
 	mat_delrow(timetable, 0);// ลบ row แรกออกไป
 	ntimetable = mat_siftrow(timetable, &timeSift);                      // กรองเวลา: เอาแค่สายที่สามารถขึ้นได้นะเวลานั้นผ่านฟังก์ชั่น timeSift()
 
 	/* นำเส้นทางเดินรถใส่เข้าไปใน vector */
-	signMap = vector_create(MAXCHAR);                      // สร้าง vector
+	signMap = vector_create(MAXCHAR);                      				// สร้าง vector
 	for(size_t i=0;i<mat_lenrow(ntimetable);i++){
 		char *busNo, *busStop;
 		int direction;
@@ -83,7 +91,11 @@ void runMain(void){
 	}
 	
 	/* plot graph */
-	graph G = graph_create(vector_size(signMap));                      // สร้าง graph มาเก็บเส้นทางเดินรถ
+	nodeNum = vector_size(signMap);
+	vector *G = (vector*)malloc(sizeof(vector)*nodeNum);                      // สร้าง graph มาเก็บเส้นทางเดินรถ
+	for(int i=0;i<nodeNum;i++){
+		G[i] = vector_create(sizeof(struct edge));
+	}
 	for(size_t i=0;i<mat_lenrow(ntimetable);i++){
 		size_t src, dest;
 		char *busNo, *busStop, *prevStop;
@@ -105,26 +117,83 @@ void runMain(void){
 
 			src = vector_find(signMap, prevStop) - vector_begin(signMap);                      // หา ID ของป้ายก่อนหน้า
 			dest = vector_find(signMap, busStop) - vector_begin(signMap);                      // หา ID ของป้ายปัจจุบัน
-			assert(src < vector_size(signMap) && dest < vector_size(signMap));
-			graph_addedge(G, src, dest, 10, busNo);                      // เพิ่ม edge ใน graph
+			vector_push(G[src], edge_create(10, dest, busNo));                     // เพิ่ม edge ใน graph
+			
 
 			prevStop = busStop;															
 		}
-
 		mat_erase(route);				
 	}
 
+	/* priority queue */
+	{
+		heap pq;
+		size_t src, dest;
+		int valid, *dist;
 
+		dist = (int*)malloc(sizeof(int)*nodeNum);
+		valid = 1;
+		pq = heap_create(sizeof(struct edge), &itemCmpr);
+		src = vector_find(signMap, userSrc) - vector_begin(signMap);
+		dest = vector_find(signMap, userDest) - vector_begin(signMap);
+		
+		if(src >= nodeNum){
+			printf("ไม่มีป้ายต้นทางดังกล่าว\n");
+			valid = 0;
+		}
+		if(dest >= nodeNum){
+			printf("ไม่มีป้ายปลายทางดังกล่าว\n");
+			valid = 0;
+		}
+
+		for(int i=0;i<nodeNum;i++){
+			dist[i] = 999999;
+		}
+		if(!valid){
+			return;	
+		}
+		dist[src] = 0;
+		heap_push(pq, edge_create(0, src, "none"));
+		while(!heap_empty(pq)){
+			edge now = (edge)heap_top(pq);
+			if(strcmp(vector_at(signMap, now->vertex), userDest) == 0){
+				break;
+			}
+			heap_pop(pq);
+			vector adj = G[now->vertex];
+			edge next;
+			while(next = (edge)vector_trav(adj)){
+				if(dist[next->vertex] > dist[now->vertex] + dist[next->weight]){
+					heap_push(pq, edge_create(now->weight + next->weight, next->vertex, next->busNo));
+					dist[next->vertex] = now->weight + next->weight;
+				}
+			}
+		}
+	}
 }
 
 void runTest(void){
-
+	
 }
 
-/* global variable */
 int keyCmpr(const void* a, const void* b){
 	return strcmp((char*)a, (char*)b);
 }
+
+int itemCmpr(const void* a, const void* b){
+	edge A = (edge)a;
+	edge B = (edge)b;
+	return A->weight > B->weight;
+}
+
+edge edge_create(int weight, size_t vertex, char* busNo){
+	edge new_edge = (edge)malloc(sizeof(struct edge));
+	new_edge->weight = weight;
+	new_edge->vertex = vertex;
+	new_edge->busNo = (char*)malloc(MAXCHAR);
+	memcpy(new_edge->busNo, busNo, MAXCHAR);
+	return new_edge;
+};
 
 /* fn เช็คว่าอยู่ในช่วงที่รถวิ่งไหม */
 int timeSift(char **a){
